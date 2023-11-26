e9fe6c8c192fd906318edb6ced97623a7c94972c
Florent Roques <florentroques@users.noreply.github.com>
2023-11-26 22:31:03 +0100

    init

---
+++ app.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+
+
+def recommend_products(df, user_id_encoded):
+    # Use TfidfVectorizer to transform the product descriptions into numerical feature vectors
+    tfidf = TfidfVectorizer(stop_words='english')
+      # fill NaN values with empty string
+    tfidf_matrix = tfidf.fit_transform(df['about_product'])
+
+    # Get the purchase history for the user
+    user_history = df[df['user_id_encoded'] == user_id_encoded]
+
+    # Use cosine_similarity to calculate the similarity between each pair of product descriptions
+    # only for the products that the user has already purchased
+    indices = user_history.index.tolist()
+
+    if indices:
+        # Create a new similarity matrix with only the rows and columns for the purchased products
+        cosine_sim_user = cosine_similarity(tfidf_matrix[indices], tfidf_matrix)
+
+        # Create a pandas Series with product indices as the index and product names as the values
+        products = df.iloc[indices]['product_name']
+        indices = pd.Series(products.index, index=products)
+
+        # Get the indices and similarity scores of products similar to the ones the user has already purchased
+        similarity_scores = list(enumerate(cosine_sim_user[-1]))
+        similarity_scores = [(i, score) for (i, score) in similarity_scores if i not in indices]
+
+        # Sort the similarity scores in descending order
+        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+
+        # Get the indices of the top 5 most similar products
+        top_products = [i[0] for i in similarity_scores[1:6]]
+
+        # Get the names of the top 5 most similar products
+        recommended_products = df.iloc[top_products]['product_name'].tolist()
+
+        # Get the reasons for the recommendation
+        score = [similarity_scores[i][1] for i in range(5)]
+
+        # Create a DataFrame with the results
+        results_df = pd.DataFrame({'Id Encoded': [user_id_encoded] * 5,
+                                   'recommended product': recommended_products,
+                                   'score recommendation': score})
+
+        return results_df
+
+    else:
+        print("No purchase history found.")
+        return None
+
+df = pd.read_csv('recommendation_db.csv')
+
+option = st.selectbox(
+    f'Choose a user ID between {df["user_id_encoded"].min()} and {df["user_id_encoded"].max()}',
+    (df['user_id_encoded'].unique())
+)
+
+st.write('You selected:', option)
+st.dataframe(recommend_products(df, 1039))
